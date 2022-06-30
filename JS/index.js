c578d8d8d069244eeb8df5e02f1d7d5649051a17
Lucas Veron <lucas_12veron@hotmail.com>
2022-06-30 20:42:01 -0300

    js

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
-let entrada = prompt("Ingresar una letra");
-let salida  = entrada + " " + "ingresada";
-alert(salida);
+var numero1 = parseInt(prompt("Ingrese la cantidad que quiere sumar en cada una de las interacciones"));
+var suma = 0;
+for(let i=0; i < 10; i++){
+    var numero2 = parseInt(prompt("Ingrese el numero que desea sumar"));
+    suma = numero1 + numero2;
+    console.log(suma);
+}
