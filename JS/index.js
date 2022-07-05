26bb382ab49c4ad0be70f4b0e8971733f6500af8
Lucas Veron <lucas_12veron@hotmail.com>
2022-07-05 20:39:04 -0300

    js3

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,48 @@
-var numero1 = parseInt(prompt("Ingrese la cantidad que quiere sumar en cada una de las interacciones"));
-var suma = 0;
-for(let i=0; i < 10; i++){
-    var numero2 = parseInt(prompt("Ingrese el numero que desea sumar"));
-    suma = numero1 + numero2;
-    console.log(suma);
+while(true){
+    var preciocosto = parseFloat(prompt("Ingrese el valor de su producto"));
+
+    if(!isNaN(preciocosto) && preciocosto != null && preciocosto !=""){
+        break;
+    }else{
+        alert('no es numero');
+        continue; 
+    }
 }
+
+    preciocostoM = preciocosto.toFixed(2);
+    alert("El valor del producto a calcular es: "+preciocostoM);
+    console.log("El valor del producto a calcular es: "+preciocostoM);
+    document.write("<h3> El valor del producto a calcular es: "+preciocostoM+"</h3>");
+
+function CalcularIva (preciocosto){
+    return (preciocosto * 0.21)
+}
+
+var iva = CalcularIva (preciocosto);
+ivaM = iva.toFixed(2);
+
+alert("El Iva del producto es: "+ivaM);
+console.log("El Iva del producto es: "+ivaM);
+document.write("<h3> El Iva del producto es: "+ivaM+"</h3>");
+
+function SumarIva(preciocosto){
+    return (preciocosto + (preciocosto * 0.21));
+}
+
+var precioconiva = SumarIva(preciocosto);
+precioconivaM = precioconiva.toFixed(2);
+
+alert("El precio con el iva es de: "+precioconivaM);
+console.log("El precio con el iva es de: "+precioconivaM);
+document.write("<h3> El precio con el iva es de: "+precioconivaM+"</h3>");
+
+function preciodeventa(precioconiva){
+    return (precioconiva * 1.25);
+}
+
+var precioventa = preciodeventa(precioconiva);
+precioventaM = precioventa.toFixed(2);
+
+alert("El precio de venta es: "+precioventaM);
+console.log("El precio de venta es: "+precioventaM);
+document.write("<h3> El precio de venta es: "+precioventaM+"</h3>")
